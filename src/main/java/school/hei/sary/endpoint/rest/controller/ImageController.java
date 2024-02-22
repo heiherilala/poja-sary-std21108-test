fa679fbdd166e95e5cde5c03fd911eabe93c8908
herilala <hei.herilala@gmail.com>
2024-02-22 11:26:30 +0300

    fix: directory

--- src/main/java/school/hei/sary/endpoint/rest/controller/ImageController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/ImageController.java
@@ -5,9 +5,8 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 import school.hei.sary.model.PicturesUTL;
 import school.hei.sary.service.PictureService;
 
@@ -18,11 +17,18 @@ public class ImageController {
 
   @PutMapping(value = "/blacks/{id}")
   public String getBlackAndWhiteImage(
-      @PathVariable(name = "id") String id,
-      @RequestParam(value = "file", required = false) MultipartFile file)
+      @PathVariable(name = "id") String id, @RequestBody(required = false) byte[] file)
       throws IOException {
-    return service.uploadAndConvertImageToBlackAndWhite(id, file.getBytes());
+    return service.uploadAndConvertImageToBlackAndWhite(id, file);
   }
+
+  //  @PutMapping(value = "/blacks/{id}")
+  //  public String getBlackAndWhiteImage(
+  //          @PathVariable(name = "id") String id,
+  //          @RequestParam(value = "file", required = false) MultipartFile file)
+  //          throws IOException {
+  //    return service.uploadAndConvertImageToBlackAndWhite(id, file.getBytes());
+  //  }
 
   @GetMapping(value = "/blacks/{id}")
   public PicturesUTL getImageURL(@PathVariable(name = "id") String id) {
